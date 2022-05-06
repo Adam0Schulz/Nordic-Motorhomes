8b3eb47ade128361790b447607ac54cc87fcc179
Adam0Schulz <adam@schulz.sk>
2022-05-06 12:32:07 +0200

    fixed employee class out of package

--- src/main/java/com/spring/nordicmotorhomes/Model/Employee.java
+++ src/main/java/com/spring/nordicmotorhomes/Model/Employee.java
@@ -1,4 +1,85 @@
 package com.spring.nordicmotorhomes.Model;
 
 public class Employee {
+
+    //ATTRIBUTES
+
+    private int employee_ID;
+    private String first_name;
+    private String last_name;
+    private int phone_number;
+    private int CPR;
+    private String title;
+    private String email;
+
+    //CONSTRUCTOR
+
+    public Employee(int employee_ID, String first_name, String last_name, int phone_number, int CPR, String title, String email) {
+        this.employee_ID = employee_ID;
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.phone_number = phone_number;
+        this.CPR = CPR;
+        this.title = title;
+        this.email = email;
+    }
+
+
+    //SETTERS AND GETTERS
+
+    public int getEmployee_ID() {
+        return employee_ID;
+    }
+
+    public void setEmployee_ID(int employee_ID) {
+        this.employee_ID = employee_ID;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public int getPhone_number() {
+        return phone_number;
+    }
+
+    public void setPhone_number(int phone_number) {
+        this.phone_number = phone_number;
+    }
+
+    public int getCPR() {
+        return CPR;
+    }
+
+    public void setCPR(int CPR) {
+        this.CPR = CPR;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
