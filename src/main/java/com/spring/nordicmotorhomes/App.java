e0e3bdf79a90c29e83d05c111dd2d4eb5ec87de2
Adam0Schulz <adam@schulz.sk>
2022-05-06 12:29:29 +0200

    added some details

--- src/main/java/com/spring/nordicmotorhomes/App.java
+++ src/main/java/com/spring/nordicmotorhomes/App.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class NordicMotorhomesApplication {
+public class App {
 
     public static void main(String[] args) {
-        SpringApplication.run(NordicMotorhomesApplication.class, args);
+        SpringApplication.run(App.class, args);
     }
 
 }
