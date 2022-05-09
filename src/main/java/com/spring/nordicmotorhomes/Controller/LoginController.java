f1dc352b9a72490879f51dfee2b35b78ba1bbdd8
Adam0Schulz <adam@schulz.sk>
2022-05-09 10:51:25 +0200

    added redirect and fixed database connection related errors

--- src/main/java/com/spring/nordicmotorhomes/Controller/LoginController.java
+++ src/main/java/com/spring/nordicmotorhomes/Controller/LoginController.java
@@ -2,13 +2,28 @@ package com.spring.nordicmotorhomes.Controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
 public class LoginController {
 
     @GetMapping("/")
+    public RedirectView index() {
+        return new RedirectView("/login");
+    }
+
+    @GetMapping("/login")
     public String login() {
         return "login/index";
+    }
+
+    @PostMapping("/login")
+    public String login(@RequestBody String string) {
+        System.out.println(string);
+
+        return "";
     }
 
 }
