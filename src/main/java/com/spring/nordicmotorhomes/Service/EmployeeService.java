6323401c2211aeed98adc31fb76bf2bea16cce3b
Adam0Schulz <adam@schulz.sk>
2022-05-09 09:26:26 +0200

    added service layer of login

--- src/main/java/com/spring/nordicmotorhomes/Service/EmployeeService.java
+++ src/main/java/com/spring/nordicmotorhomes/Service/EmployeeService.java
@@ -1,7 +1,27 @@
 package com.spring.nordicmotorhomes.Service;
 
+import com.spring.nordicmotorhomes.DAO.EmployeeDAO;
+import com.spring.nordicmotorhomes.Model.Employee;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
 
 @Service
 public class EmployeeService {
+
+    public Employee Login(String username, String password) {
+
+        Employee employee = null;
+        ArrayList<Employee> employees = EmployeeDAO.fetchAllEmployees();
+
+        for (Employee emp : employees) {
+            if(emp.getEmail().equals(username) || emp.getPassword().equals(password)) {
+                employee = emp;
+                break;
+            }
+        }
+
+        return employee;
+    }
+
 }
