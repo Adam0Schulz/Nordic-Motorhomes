4d2a938c9370b89ae0d578ea82b1da5a0041b2ca
simonatingleffkardel <simonakardel@gmail.com>
2022-05-18 12:42:35 +0200

    wrote html for dashboard, uploaded images

--- src/main/java/com/spring/nordicmotorhomes/Controller/SAController.java
+++ src/main/java/com/spring/nordicmotorhomes/Controller/SAController.java
@@ -5,14 +5,6 @@ import com.spring.nordicmotorhomes.Service.SAService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
-import java.sql.Date;
-import java.sql.Time;
-import java.text.SimpleDateFormat;
-import java.util.HashSet;
-import java.util.Set;
 
 @Controller
 public class SAController {
@@ -23,7 +15,7 @@ public class SAController {
     @GetMapping("/dashboard/sa")
     public String index () {
         if(EmployeeService.getCurrentEmp().getTitle().equals("sales assistant")) {
-            return "dashboard/sa/index";
+            return "dashboard";
         }
         return "redirect:/login";
     }
