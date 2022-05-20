1dcbd0a3f0e1075c8ceff216eb2ccd6d64a924f4
abalo <emie0038>
2022-05-20 09:12:06 +0200

    database

--- SQL/DMLBookings.sql
+++ SQL/DMLBookings.sql
@@ -5,9 +5,6 @@ USE nordic_motorhomes;
 INSERT INTO nordic_motorhomes.bookings
    (ID, customer_ID, start_date, pick_up_time, end_date, motorhome_ID,
      employee_ID, pick_up_loc, drop_off_loc, total_price, fuel_level)
-         (SELECT * FROM nordic_motorhomes.bookings as b
-         LEFT JOIN customers c on b.customer_ID = c.ID
-         LEFT JOIN nordic_motorhomes.motorhomes as m on b.motorhome_ID = m.ID
-         LEFT JOIN nordic_motorhomes.employees as e on b.employee_ID = e.ID);
+VALUES(ID, 1, date(10/10/2022), time(10),date(10/20/2022), 1, 1, 'Hellerup', 'Gentofte',5000, 400);
 
 
